05bf4f37012b244bc322cc0a396137e6b94999f6
Iván García <77176510+ivanGarciaBernal@users.noreply.github.com>
2023-11-22 14:40:03 -0600

    Update __init__.py

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -1,8 +1,13 @@
-def my_sum(x, y):
-    """A function that sums. """
-    return x+y
+from sympy import sqrt
 
+def es_potencia(k,n):
+    return int(k**(1/n)) ** n == k
 
-def my_mul(x, y):
-    """A function that multiply. """
-    return x*y
+def potencia_fibonacci(n, pot=2):
+    fibs = []
+    a, b = 0, 1
+    while a < n:
+        if es_potencia(a,pot):
+            fibs.append(a)
+        a, b = b, a+b
+    return fibs
